d022d8ea043dd00cdfa2da62262ffbc0c93a2b49
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2023-09-13 15:21:51 +0500

    Ads Integrated banner

---
+++ android/app/src/main/kotlin/com/example/bmi_ads_calc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmi_ads_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'homepage.dart';
+
+void main() {
+  // 2 things to do for ads running
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
+  // //
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bmi calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+
+        //Text Themes
+        //
+        textTheme: TextTheme(
+            //Body Small
+            bodySmall: GoogleFonts.roboto(
+                fontSize: 14, color: const Color(0xff39AFEA)),
+            //Body Medium
+            bodyMedium: GoogleFonts.roboto(
+                fontSize: 16, fontWeight: FontWeight.w700, color: Colors.black),
+            //
+            //Body Large
+            bodyLarge: GoogleFonts.roboto(
+                fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black),
+            //title Medium
+            titleMedium: GoogleFonts.montserrat(
+                fontSize: 24, color: Colors.black, fontWeight: FontWeight.w700),
+            //title Large
+            titleLarge: GoogleFonts.montserrat(
+                fontSize: 34,
+                fontWeight: FontWeight.w700,
+                color: const Color(0xff39AFEA))),
+
+        //
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
